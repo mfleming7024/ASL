cb49da4008e4217d40349bc529fdd278f85eaf2a
eangeles <niwre2007@gmail.com>
2013-07-16 14:51:24 -0400

    beta 0.1

--- darkroom.com/application/views/includes/footer.php
+++ darkroom.com/application/views/includes/footer.php
@@ -1,9 +1,6 @@
 
 
 
-<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
-<script type="text/javascript" src="js/lightbox-2.6.min.js"></script>
-
 
 <!-- End Document
 ================================================== -->
